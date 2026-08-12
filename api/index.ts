f70ff833af6d0b30b883c6a5091c6b9be9271246
Gm-altadmin <Gm@belkagolf.com>
2026-08-12 22:23:37 +0300

    Update import statement to include .js extension

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import { createApp } from "../server/_core/app";
+import { createApp } from "../server/_core/app.js";
 
 /**
  * Vercel Node.js serverless giriş noktası.
